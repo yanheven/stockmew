7aee79ff6217e19f9a27ae54888174a7bda0e4a2
abacan <abacan@4a1a3b49-ba4d-0410-a375-7fbb4e06d271>
2008-05-23 15:17:49 +0000

    增加接收服务器端数据的超时的控制，以解决部分手机执行recv 函数死机的现象。

--- branches/wmdependent/src/http-retriver/include/http-retriver-utils.h
+++ branches/wmdependent/src/http-retriver/include/http-retriver-utils.h
@@ -8,4 +8,6 @@ struct string_element{
 
 int split_string_by_char(struct string_element *string_array, char *string, char split_char);
 
+int run_with_timeout(double seconds, void (*fun) (void *), void *arg);
+
 #endif /* UTILS_H */
--- branches/wmdependent/src/http-retriver/include/http-retriver.h
+++ branches/wmdependent/src/http-retriver/include/http-retriver.h
@@ -2,6 +2,7 @@
 #define HTTP_RETRIVER_H
 
 #include "http-body-parse.h"
+
 int retrieve_stock_price(struct stock_price* price_info, char* url, parse_body_string_fun parse_body_string);
 
 #endif /* HTTP_RETRIVER_H */
